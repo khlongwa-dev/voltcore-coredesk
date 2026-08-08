a94881c99f531e58f25101ea5e087c6560fae61a
khlongwa-dev <ayandahlongwa21@gmail.com>
2026-08-08 13:28:26 +0200

    fix: fixed a bug in main.py. Cors middleware were not configured resulting in frontend being unable to reach the backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from  fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import Base, engine
 from app.api import auth, tickets, comments, users
 
@@ -6,6 +8,14 @@ app = FastAPI(
 	title="Coredesk",
 	description="IT helpdesk for Voltcore Engineering Solutions",
 	version="1.0.0"
+)
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
 )
 
 app.include_router(auth.router)
